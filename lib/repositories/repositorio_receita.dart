6c5960a77c3a65f7c1d344c534f7b936647d420a
Maria Clara <mclaradearaujop@aluno.uespi.br>
2024-11-24 18:08:14 -0300

    feat: chamada da api

---
+++ lib/repositories/repositorio_receita.dart
@@ -0,0 +1,17 @@
+﻿import 'package:dio/dio.dart';
+import 'package:receitoteca/models/receita.dart';
+
+class ReceitaRepositorio {
+  final Dio dio = Dio();
+  final String url = 'https://www.themealdb.com/api/json/v1/1/random.php';
+  Receita? receita;
+
+  Future<void> getReceita() async{
+    try {
+      final response = await dio.get(url);
+      receita = Receita.fromJson(response.data);
+    } catch (e) {
+      print('Erro ao buscar receita: $e');
+    }
+  }
+}
