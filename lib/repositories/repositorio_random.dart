f0026c5e025adc3af7123863f9550704da005270
Maria Clara <mclaradearaujop@aluno.uespi.br>
2024-12-02 20:52:59 -0300

    feat: criação do repositório de random

---
+++ lib/repositories/repositorio_random.dart
@@ -0,0 +1,18 @@
+﻿import 'package:dio/dio.dart';
+import 'package:receitoteca/models/receita.dart';
+
+class AleatoriaRepositorio {
+  final Dio dio = Dio();
+  Receita? receita;
+
+  String get url => 'https://www.themealdb.com/api/json/v1/1/random.php'; 
+
+  Future<void> getReceita() async {
+    try {
+      final response = await dio.get(url);
+      receita = Receita.fromJson(response.data);
+    } catch (e) {
+      print('Erro ao buscar receita: $e');
+    }
+  }
+}
