eb2d528e4f5814b4fb1b98e8a8f7765f71e5af07
Maria Clara <mclaradearaujop@aluno.uespi.br>
2024-12-02 20:54:14 -0300

    feat: criação do widget de imagem da receita

---
+++ lib/widgets/image_revenue.dart
@@ -0,0 +1,55 @@
+﻿import 'package:flutter/material.dart';
+import 'package:receitoteca/models/receita.dart';
+
+class ImageRevenue extends StatelessWidget {
+  final Receita? receita;
+  const ImageRevenue({super.key, required this.receita});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        AspectRatio(
+          aspectRatio: 16 / 9,
+          child: Image.network(
+            receita!.meals!.first.strMealThumb ?? '',
+            fit: BoxFit.cover,
+          ),
+        ),
+        Positioned.fill(
+          child: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Colors.transparent,
+                  Colors.black54,
+                ],
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 10,
+          left: 10,
+          child: Text(
+            receita!.meals!.first.strMeal ?? '',
+            style: const TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+              shadows: [
+                Shadow(
+                  blurRadius: 10.0,
+                  color: Colors.black,
+                  offset: Offset(2, 2),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
