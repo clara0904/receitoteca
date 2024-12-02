b855bc043a987cc5360fabcb4bc3704082fc124b
Maria Clara <mclaradearaujop@aluno.uespi.br>
2024-12-02 20:53:28 -0300

    feat: adição de receita do dia na home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,8 @@
 ﻿import 'package:flutter/material.dart';
 import 'package:receitoteca/models/receita.dart';
-import 'package:receitoteca/repositories/repositorio_receita.dart';
+import 'package:receitoteca/repositories/repositorio_random.dart';
+import 'package:receitoteca/screens/revenue_screen.dart';
+import 'package:receitoteca/widgets/image_revenue.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,7 +12,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final ReceitaRepositorio repository = ReceitaRepositorio();
+  final AleatoriaRepositorio repository = AleatoriaRepositorio();
   Receita? receita;
 
   @override
@@ -35,31 +37,41 @@ class _HomeScreenState extends State<HomeScreen> {
             'RECEITOTECA',
             style: TextStyle(
               fontSize: 25,
-              fontWeight: FontWeight.w600
+              fontWeight: FontWeight.w600,
+              color: Colors.white
             ),
           ),
           centerTitle: true,
-          backgroundColor: const Color.fromARGB(255, 214, 213, 213),
+          backgroundColor: const Color.fromARGB(255, 4, 43, 77),
         ),
         body: receita == null
           ? const Center(child: CircularProgressIndicator())
           : SingleChildScrollView(
             child: Padding(
-                padding: const EdgeInsets.all(10.0),
+                padding: const EdgeInsets.all(8.0),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text('Nome da receita: ${receita!.meals!.first.strMeal}', style: const TextStyle(fontSize: 20)),
-                    AspectRatio(
-                      aspectRatio: 16 / 9,
-                        child: Image.network(
-                          receita!.meals!.first.strMealThumb ?? '',
-                          height: 200.0,
-                          width: double.infinity,
-                          fit: BoxFit.cover,
-                        ),
+                    const Text('Receita do dia ', style: TextStyle(fontSize: 24, color: Colors.white,
+                    shadows: [
+                      Shadow(
+                        blurRadius: 10.0,
+                        color: Colors.black,
+                        offset: Offset(2, 2),
+                      ),
+                    ])
                     ),
-                    Text('Instruções: \n${receita!.meals!.first.strInstructions}', style: const TextStyle(fontSize: 16)),
+                    TextButton(
+                      onPressed: () { 
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (_) => ReceitaScreen(endpoint: receita!.meals!.first.idMeal!)
+                          )
+                        );
+                      }, 
+                      child: ImageRevenue(receita: receita)
+                    )
                   ],
                 ),
               ),
