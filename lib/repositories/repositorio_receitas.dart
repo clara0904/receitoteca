ba4b3cb5a202c468400517a044e48ff367e5b35a
Maria Clara <mclaradearaujop@aluno.uespi.br>
2024-12-14 18:37:59 -0300

    feat: criação do repositório de receitas(lista)

---
+++ lib/repositories/repositorio_receitas.dart
@@ -0,0 +1,23 @@
+﻿import 'package:dio/dio.dart';
+import 'package:receitoteca/models/receitas.dart';
+
+class RepositorioReceitas {
+  final String endpoint;
+  final Dio dio = Dio();
+
+  RepositorioReceitas(this.endpoint);
+
+  String get url => 'https://www.themealdb.com/api/json/v1/1/filter.php?c=$endpoint';
+
+  Future<List<Meals>> getReceitas() async {
+  try {
+    final response = await dio.get(url);
+    List<dynamic> data = response.data['meals'];
+
+    return data.map((json) => Meals.fromJson(json)).toList();
+  } catch (e) {
+    print('Erro ao buscar receitas: $e');
+    return [];
+  }
+}
+}
