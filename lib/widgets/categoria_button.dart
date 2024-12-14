338d2c7e10b39860e1328678e36e085869e8844a
Maria Clara <mclaradearaujop@aluno.uespi.br>
2024-12-14 20:09:34 -0300

    refactor: mudança completa no estilo dos botões de categorias

---
+++ lib/widgets/categoria_button.dart
@@ -0,0 +1,55 @@
+﻿import 'package:flutter/material.dart';
+import 'package:receitoteca/models/categorias.dart';
+import 'package:receitoteca/screens/list_revenue_screen.dart';
+
+class CategoriaButton extends StatelessWidget {
+  final List<Categories> categorias;
+
+  const CategoriaButton({super.key, required this.categorias});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Wrap(
+        spacing: 10.0, 
+        children: categorias.map((categoria) {
+          return InkWell(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (_) => ListRevenueScreen(endpoint: categoria.strCategory ?? ''),
+                ),
+              );
+            },
+            child: Chip(
+              backgroundColor: const Color.fromARGB(255, 255, 254, 171),
+              padding: const EdgeInsets.all(4.0),
+              label: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Image.network(
+                    categoria.strCategoryThumb ?? '', 
+                    width: 70, 
+                    height: 70, 
+                    fit: BoxFit.contain,
+                  ),
+                  const SizedBox(height: 6.0), 
+                  Text(
+                    categoria.strCategory ?? '', 
+                    style: const TextStyle(
+                      color: Color.fromARGB(255, 63, 63, 63),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
