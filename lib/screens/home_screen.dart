1ea73b6152ab8835b9d6d227be11293825706b53
Maria Clara <mclaradearaujop@aluno.uespi.br>
2024-12-14 18:40:12 -0300

    feat: adição de botões de categorias na home

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,10 @@
 ﻿import 'package:flutter/material.dart';
+import 'package:receitoteca/models/categorias.dart';
 import 'package:receitoteca/models/receita.dart';
+import 'package:receitoteca/repositories/repositorio_categoria.dart';
 import 'package:receitoteca/repositories/repositorio_random.dart';
 import 'package:receitoteca/screens/revenue_screen.dart';
+import 'package:receitoteca/widgets/categoria_button.dart';
 import 'package:receitoteca/widgets/image_revenue.dart';
 import 'package:receitoteca/widgets/random_revenue.dart';
 
@@ -14,18 +17,29 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final AleatoriaRepositorio repository = AleatoriaRepositorio();
+  final RepositorioCategoria repositorio = RepositorioCategoria();
+  
+  List<Categories> categorias = [];
   Receita? receita;
 
   @override
   void initState() {
     super.initState();
     fetchData();
+    fetchCategorias();
   }
 
   void fetchData() async {
     await repository.getReceita();
     setState(() {
-      receita = repository.receita; 
+      receita = repository.receita;
+    });
+  }
+
+  void fetchCategorias() async {
+    List<Categories> fetchedCategorias = await repositorio.getCategorias();
+    setState(() {
+      categorias = fetchedCategorias;
     });
   }
 
@@ -39,46 +53,66 @@ class _HomeScreenState extends State<HomeScreen> {
             style: TextStyle(
               fontSize: 25,
               fontWeight: FontWeight.w600,
-              color: Colors.white
+              color: Colors.white,
             ),
           ),
           centerTitle: true,
           backgroundColor: const Color.fromARGB(255, 4, 43, 77),
         ),
         body: receita == null
-          ? const Center(child: CircularProgressIndicator())
-          : SingleChildScrollView(
-            child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text('Receita do dia ', style: TextStyle(fontSize: 24, color: Colors.white,
-                    shadows: [
-                      Shadow(
-                        blurRadius: 10.0,
-                        color: Colors.black,
-                        offset: Offset(2, 2),
+            ? const Center(child: CircularProgressIndicator())
+            : SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Receita do dia',
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.white,
+                          shadows: [
+                            Shadow(
+                              blurRadius: 10.0,
+                              color: Colors.black,
+                              offset: Offset(2, 2),
+                            ),
+                          ],
+                        ),
                       ),
-                    ])
-                    ),
-                    TextButton(
-                      onPressed: () { 
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (_) => ReceitaScreen(endpoint: receita!.meals!.first.idMeal!)
-                          )
-                        );
-                      }, 
-                      child: ImageRevenue(receita: receita, visibleName: true, visibleStyle: true,)
-                    ),
-                    const SizedBox(height: 20,),
-                    const SorteadorReceita()
-                  ],
+                      TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (_) => ReceitaScreen(
+                                endpoint: receita!.meals!.first.idMeal!,
+                              ),
+                            ),
+                          );
+                        },
+                        child: ImageRevenue(
+                          receita: receita,
+                          visibleName: true,
+                          visibleStyle: true,
+                        ),
+                      ),
+                    const SizedBox(height: 20),
+                      categorias.isNotEmpty
+                          ? CategoriaButton(categorias: categorias,)
+                          : const Center(
+                              child: Text(
+                                'Nenhuma categoria encontrada.',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                      const SizedBox(height: 20),
+                      const SorteadorReceita(),
+                    ],
+                  ),
                 ),
               ),
-          ),
       ),
     );
   }
