6917d6ee0933b750efbb82400a4f2a2bf1e02e96
Maria Clara <mclaradearaujop@aluno.uespi.br>
2024-12-14 20:10:01 -0300

    refactor: atualização de imagem de receita

--- lib/widgets/image_revenue.dart
+++ lib/widgets/image_revenue.dart
@@ -3,57 +3,52 @@ import 'package:receitoteca/models/receita.dart';
 
 class ImageRevenue extends StatelessWidget {
   final Receita? receita;
-  final bool visibleName;
-  final bool visibleStyle;
-  const ImageRevenue({super.key, required this.receita, required this.visibleName, required this.visibleStyle});
+  const ImageRevenue({super.key, required this.receita});
 
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        Image.network(
-          receita!.meals!.first.strMealThumb ?? '',
-          fit: BoxFit.cover,
-          width: double.infinity,
+        AspectRatio(
+          aspectRatio: 4/3,
+          child: Image.network(
+            receita!.meals!.first.strMealThumb ?? '',
+            fit: BoxFit.cover,
+            width: double.infinity,
+          ),
         ),
-        Visibility(
-          visible: visibleStyle,
-          child: Positioned.fill(
-            child: Container(
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: [
-                    Colors.transparent,
-                    Colors.black54,
-                  ],
-                ),
+        Positioned.fill(
+          child: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Colors.transparent,
+                  Colors.black54,
+                ],
               ),
             ),
           ),
         ),
-        Visibility(
-          visible: visibleName,
-          child: Positioned(
-            bottom: 10,
-            left: 10,
-            right: 10,
-            child: Text(
-              receita!.meals!.first.strMeal ?? '',
-              overflow: TextOverflow.ellipsis,
-              style: const TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-                shadows: [
-                  Shadow(
-                    blurRadius: 10.0,
-                    color: Colors.black,
-                    offset: Offset(2, 2),
-                  ),
-                ],
-              ),
+        Positioned(
+          bottom: 10,
+          left: 10,
+          right: 10,
+          child: Text(
+            receita!.meals!.first.strMeal ?? '',
+            overflow: TextOverflow.ellipsis,
+            style: const TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+              shadows: [
+                Shadow(
+                  blurRadius: 10.0,
+                  color: Colors.black,
+                  offset: Offset(2, 2),
+                ),
+              ],
             ),
           ),
         ),
