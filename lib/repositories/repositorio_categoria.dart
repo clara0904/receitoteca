74ca1b6741ed502d188f8749a658abfceac0dcc9
Maria Clara <mclaradearaujop@aluno.uespi.br>
2024-12-14 18:37:26 -0300

    feat: criação do repositório de categorias

---
+++ lib/repositories/repositorio_categoria.dart
@@ -0,0 +1,20 @@
+﻿import 'package:dio/dio.dart';
+import 'package:receitoteca/models/categorias.dart';
+
+class RepositorioCategoria {
+  final Dio dio = Dio();
+
+  String get url => 'https://www.themealdb.com/api/json/v1/1/categories.php';
+
+  Future<List<Categories>> getCategorias() async {
+    try {
+      final response = await dio.get(url);
+      List<dynamic> data = response.data['categories'];
+
+      return data.map((json) => Categories.fromJson(json)).toList();
+    } catch (e) {
+      print('Erro ao buscar categorias: $e');
+      return [];
+    }
+  }
+}
