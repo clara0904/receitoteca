a8bcdbb9a3951b8a3c229e34b6ff8df3fd876732
Maria Clara <mclaradearaujop@aluno.uespi.br>
2024-12-14 18:39:03 -0300

    feat: criação da tela que lista receitas por categoria

---
+++ lib/screens/list_revenue_screen.dart
@@ -0,0 +1,88 @@
+﻿import 'package:flutter/material.dart';
+import 'package:receitoteca/repositories/repositorio_receitas.dart';
+import 'package:receitoteca/models/receitas.dart';
+import 'package:receitoteca/screens/revenue_screen.dart';
+
+class ListRevenueScreen extends StatefulWidget {
+  final String endpoint;
+  const ListRevenueScreen({super.key, required this.endpoint});
+
+  @override
+  State<ListRevenueScreen> createState() => _ListRevenueScreenState();
+}
+
+class _ListRevenueScreenState extends State<ListRevenueScreen> {
+  late final RepositorioReceitas repositoryReceitas;
+  List<Meals> receitas = [];
+
+  @override
+  void initState() {
+    super.initState();
+    repositoryReceitas = RepositorioReceitas(widget.endpoint);
+    fetchReceitas();
+  }
+
+ void fetchReceitas() async {
+  List<Meals> fetchedReceitas = await repositoryReceitas.getReceitas();
+  setState(() {
+    receitas = fetchedReceitas;
+  });
+}
+
+@override
+Widget build(BuildContext context) {
+  return SafeArea(
+    child: Scaffold(
+      appBar: AppBar(
+        title: Text(widget.endpoint),
+        centerTitle: true,
+      ),
+      body: receitas.isEmpty
+          ? const Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              itemCount: receitas.length,
+              itemBuilder: (context, index) {
+                final meal = receitas[index];
+                return TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) =>  ReceitaScreen(endpoint: meal.idMeal ?? '',)
+                      )
+                    );
+                  },
+                  child: Card(
+                    margin: const EdgeInsets.symmetric(vertical: 0, horizontal: 0),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12), 
+                    ),
+                    elevation: 5,
+                    child: ListTile(
+                      contentPadding: const EdgeInsets.all(0),
+                      leading: ClipRRect(
+                        borderRadius: BorderRadius.circular(12),
+                        child: Image.network(
+                          meal.strMealThumb ?? '',
+                          width: 80,
+                          height: 80,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      title: Padding(
+                        padding: const EdgeInsets.only(right: 8),
+                        child: Text(
+                          meal.strMeal ?? 'Sem título',
+                          style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+    ),
+  );
+  }
+}
