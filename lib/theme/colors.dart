8777b6ebbcdcbc30cf2988a34a90b9b28c1f5fb6
Maria Clara <mclaradearaujop@aluno.uespi.br>
2024-12-15 20:28:02 -0300

    feat: criação da classe colors para todo o app

---
+++ lib/theme/colors.dart
@@ -0,0 +1,13 @@
+﻿import 'package:flutter/material.dart';
+
+class ColorsApp{
+  static Color backgroundPrimario = const Color(0xFFDBDAD5);
+  static Color backgroundVerde = const Color(0xFF9AB5AE);
+  static Color backgroundAppBar = const Color(0xFF4C6665);
+  static Color backgroundCard = const Color(0xFFA26D5D);
+  static Color backgroundSecundario = const Color.fromARGB(255, 168, 106, 106);
+  static Color backgroundTerciario = const Color.fromARGB(255, 139, 32, 32);
+  static Color corFontePrimaria = const Color.fromARGB(255, 135, 49, 22);
+  static Color corFonteSecundaria = const Color.fromARGB(207, 255, 255, 255);
+  static Color corFonteTerciaria = const Color.fromARGB(255, 31, 90, 88);
+}
