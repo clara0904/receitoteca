5deea9c635998fc1a124af10970f570b6fe711bd
Maria Clara <mclaradearaujop@aluno.uespi.br>
2024-12-16 10:52:57 -0300

    feat: criação de título padrão para o app

---
+++ lib/widgets/title.dart
@@ -0,0 +1,27 @@
+﻿import 'package:flutter/material.dart';
+import 'package:receitoteca/theme/colors.dart';
+
+class TemaTitulo extends StatelessWidget {
+  final String titulo;
+  final double size;
+  const TemaTitulo({super.key, required this.titulo, required this.size});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      titulo,
+      style: TextStyle(
+        fontSize: size,
+        color: ColorsApp.corFontePrimaria,
+        fontWeight: FontWeight.w600,
+        shadows: [
+          Shadow(
+            blurRadius: 8.0,
+            color: Colors.black.withOpacity(0.3),
+            offset: const Offset(2, 2),
+          ),
+        ],
+      ),
+    );
+  }
+}
