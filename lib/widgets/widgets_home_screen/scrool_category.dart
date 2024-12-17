e9045b6ef3375e114eb60ac333a98285658299fc
Maria Clara <mclaradearaujop@aluno.uespi.br>
2024-12-17 15:45:15 -0300

    feat: criação do widget scrool_category

---
+++ lib/widgets/widgets_home_screen/scrool_category.dart
@@ -0,0 +1,30 @@
+﻿import 'package:flutter/material.dart';
+import 'package:receitoteca/models/categorias.dart';
+import 'package:receitoteca/widgets/widgets_home_screen/categoria_button.dart';
+import 'package:receitoteca/widgets/theme_title.dart';
+
+class ScroolCategory extends StatelessWidget {
+  final List<Categories> categorias;
+  const ScroolCategory({super.key, required this.categorias});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const TemaTitulo(titulo: 'Categorias: ', size: 22),
+          categorias.isNotEmpty
+            ? Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: CategoriaButton(categorias: categorias,),
+            )
+            : const Center(
+                child: CircularProgressIndicator(),
+              ),
+        ],
+      ),
+    );
+  }
+}
