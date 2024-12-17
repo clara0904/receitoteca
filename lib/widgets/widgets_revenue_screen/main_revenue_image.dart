f57f8d12c27c3faaa81905e2a04f81c173f711f4
Maria Clara <mclaradearaujop@aluno.uespi.br>
2024-12-17 15:46:08 -0300

    feat: criação do widget main_revenue_image

---
+++ lib/widgets/widgets_revenue_screen/main_revenue_image.dart
@@ -0,0 +1,33 @@
+﻿import 'package:flutter/material.dart';
+import 'package:receitoteca/models/receita.dart';
+import 'package:receitoteca/widgets/theme_title.dart';
+
+class ImagemPrincipalReceita extends StatelessWidget {
+  final Receita? receita;
+  const ImagemPrincipalReceita({super.key, required this.receita});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            margin: const EdgeInsets.only(bottom: 8),
+            child: 
+            TemaTitulo(
+              titulo: '${receita!.meals!.first.strMeal}', 
+              size: 32,
+            ),
+          ),
+          Image.network(
+            receita!.meals!.first.strMealThumb ?? '',
+            fit: BoxFit.cover,
+            width: double.infinity,
+          ),
+        ],
+      ),
+    );
+  }
+}
