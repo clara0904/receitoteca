3e34725518fb09d5bd3190dc488085c805b78539
Maria Clara <mclaradearaujop@aluno.uespi.br>
2024-12-17 15:46:39 -0300

    feat: criação do widget revenue_instructions

---
+++ lib/widgets/widgets_revenue_screen/revenue_instructions.dart
@@ -0,0 +1,31 @@
+﻿import 'package:flutter/material.dart';
+import 'package:receitoteca/models/receita.dart';
+import 'package:receitoteca/theme/colors.dart';
+
+class InstrucoesReceita extends StatelessWidget {
+  final Receita? receita;
+  const InstrucoesReceita({super.key, required this.receita});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Instruções:',
+            style: TextStyle(fontSize: 20, 
+              color: ColorsApp.corFontePrimaria,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          Text(
+            '${receita!.meals!.first.strInstructions}',
+            style: TextStyle(fontSize: 18, color: ColorsApp.corFontePrimaria),
+          ),
+        ],
+      ),
+    );
+  }
+}
