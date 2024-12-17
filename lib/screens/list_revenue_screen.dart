b6d8842db4323e2acae5a21c711bc812c08d8487
Maria Clara <mclaradearaujop@aluno.uespi.br>
2024-12-17 15:31:44 -0300

    refactor: extração de método na list_revenue_screen

--- lib/screens/list_revenue_screen.dart
+++ lib/screens/list_revenue_screen.dart
@@ -1,8 +1,8 @@
 ﻿import 'package:flutter/material.dart';
 import 'package:receitoteca/repositories/repositorio_receitas.dart';
-import 'package:receitoteca/models/receitas.dart';
+import 'package:receitoteca/models/lista_receitas.dart';
 import 'package:receitoteca/screens/revenue_screen.dart';
-import 'package:receitoteca/theme/colors.dart';
+import 'package:receitoteca/widgets/card_list_revenue.dart';
 
 class ListRevenueScreen extends StatefulWidget {
   final String endpoint;
@@ -32,62 +32,31 @@ class _ListRevenueScreenState extends State<ListRevenueScreen> {
 
 @override
 Widget build(BuildContext context) {
-  return SafeArea(
-    child: Scaffold(
-      appBar: AppBar(
-        title: Text(widget.endpoint,),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(widget.endpoint,),
+        ),
+        body: receitas.isEmpty
+            ? const Center(child: CircularProgressIndicator())
+            : ListView.builder(
+                itemCount: receitas.length,
+                itemBuilder: (context, index) {
+                  final meal = receitas[index];
+                  return TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (_) =>  ReceitaScreen(endpoint: meal.idMeal ?? '',)
+                        )
+                      );
+                    },
+                    child: CardListaReceitas(meal: meal),
+                  );
+                },
+              ),
       ),
-      body: receitas.isEmpty
-          ? const Center(child: CircularProgressIndicator())
-          : ListView.builder(
-              itemCount: receitas.length,
-              itemBuilder: (context, index) {
-                final meal = receitas[index];
-                return TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (_) =>  ReceitaScreen(endpoint: meal.idMeal ?? '',)
-                      )
-                    );
-                  },
-                  child: Card(
-                    color: ColorsApp.backgroundCard,
-                    margin: const EdgeInsets.symmetric(vertical: 0, horizontal: 0),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12), 
-                    ),
-                    elevation: 5,
-                    child: ListTile(
-                      contentPadding: const EdgeInsets.all(0),
-                      leading: ClipRRect(
-                        borderRadius: BorderRadius.circular(12),
-                        child: Image.network(
-                          meal.strMealThumb ?? '',
-                          width: 80,
-                          height: 80,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      title: Padding(
-                        padding: const EdgeInsets.only(right: 8),
-                        child: Text(
-                          meal.strMeal ?? 'Sem título',
-                          style: TextStyle(
-                            fontSize: 18, 
-                            fontWeight: FontWeight.bold,
-                            color: ColorsApp.corFonteSecundaria,
-                          ),
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                      ),
-                    ),
-                  ),
-                );
-              },
-            ),
-    ),
-  );
+    );
   }
 }
