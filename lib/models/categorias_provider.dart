fc185ebe735f73c0b0de101c7c44c6f2f9ec04f5
Maria Clara <mclaradearaujop@aluno.uespi.br>
2024-12-18 10:12:41 -0300

    feat: criação de model categorias_provider

---
+++ lib/models/categorias_provider.dart
@@ -0,0 +1,13 @@
+﻿import 'package:flutter/material.dart';
+import 'package:receitoteca/models/categorias.dart';
+import 'package:receitoteca/repositories/repositorio_categoria.dart';
+
+class CategoriasProvider extends ChangeNotifier{
+  final RepositorioCategoria _repositorioCategoria = RepositorioCategoria();
+  List<Categories> categorias = [];
+
+  Future<void> fetchCategorias() async {
+    categorias = await _repositorioCategoria.getCategorias(); 
+    notifyListeners();
+  }
+}
