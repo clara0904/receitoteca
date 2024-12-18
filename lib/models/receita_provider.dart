152952953c207cb9efe4393bde511d565abacbbb
Maria Clara <mclaradearaujop@aluno.uespi.br>
2024-12-18 10:12:22 -0300

    feat: criação de model receita_provider

---
+++ lib/models/receita_provider.dart
@@ -0,0 +1,16 @@
+﻿import 'package:flutter/material.dart';
+import 'package:receitoteca/models/receita.dart';
+import 'package:receitoteca/repositories/repositorio_random.dart';
+
+class ReceitaProvider extends ChangeNotifier{
+  final AleatoriaRepositorio _repositorio = AleatoriaRepositorio();
+  Receita? _receita;
+
+  Receita? get receita => _receita;
+
+  Future<void> fetchReceita() async {
+    await _repositorio.getReceita();
+    _receita = _repositorio.receita;
+    notifyListeners();
+  }
+}
