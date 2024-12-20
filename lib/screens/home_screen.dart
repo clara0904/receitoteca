5f8a8ce23c050ab36bf6f70ce997fcb828876741
Maria Clara <mclaradearaujop@aluno.uespi.br>
2024-12-20 15:25:30 -0300

    refactor: adição do provider em home_screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,7 @@
 ﻿import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:receitoteca/models/categorias_provider.dart';
-import 'package:receitoteca/models/receita_provider.dart';
+import 'package:receitoteca/models/provider/categorias_provider.dart';
+import 'package:receitoteca/models/provider/receita_random_provider.dart';
 import 'package:receitoteca/widgets/widgets_home_screen/random_revenue.dart';
 import 'package:receitoteca/widgets/widgets_home_screen/recomendation.dart';
 import 'package:receitoteca/widgets/widgets_home_screen/scrool_category.dart';
@@ -14,22 +14,25 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
   @override
   void initState() {
     super.initState();
-    final receitaProvider = Provider.of<ReceitaProvider>(context, listen: false);
+    fetchData();
+  }
+
+  void fetchData() {
+    final receitaProvider = Provider.of<ReceitaRandomProvider>(context, listen: false);
     final categoriasProvider = Provider.of<CategoriasProvider>(context, listen: false);
-      Future.microtask((){
-        receitaProvider.fetchReceita();
-        categoriasProvider.fetchCategorias();
-      }
-    );
+
+    Future.microtask(() {
+      receitaProvider.fetchReceita();
+      categoriasProvider.fetchCategorias();
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    final receitaProvider = Provider.of<ReceitaProvider>(context);
+    final receitaProvider = Provider.of<ReceitaRandomProvider>(context);
     final categoriaProvider = Provider.of<CategoriasProvider>(context);
 
     return SafeArea(
@@ -40,22 +43,24 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ),
         body: receitaProvider.receita == null
-          ? const Center(child: CircularProgressIndicator())
-          : SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
-                child: Column(
-                  children: [
+            ? const Center(child: CircularProgressIndicator())
+            : SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
+                  child: Column(
+                    children: [
+                      RecomendacaoReceita(receita: receitaProvider.receita),
 
-                  RecomendacaoReceita(receita:  receitaProvider.receita,),
+                      ScroolCategory(categorias: categoriaProvider.categorias),
 
-                  ScroolCategory(categorias: categoriaProvider.categorias,),
-
-                  const SorteadorReceita(),
-                  ],
+                      ChangeNotifierProvider(
+                        create: (_) => ReceitaRandomProvider(),
+                        child: const SorteadorReceita(),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
       ),
     );
   }
